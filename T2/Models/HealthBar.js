6407c1510e11c7eb1c5574c4db89ec52435ac787
Arthur-lc <costa.arthur.lima@gmail.com>
2024-08-19 17:32:19 -0300

    healthbar funcionando no chrome

--- T2/Models/HealthBar.js
+++ T2/Models/HealthBar.js
@@ -5,17 +5,17 @@ class HealthBar extends CSS2DObject {
     constructor(max) {
         const progressBarElement = document.createElement( 'progress' );
         progressBarElement.className = 'healthBar';
-        
         progressBarElement.max = max;
+        progressBarElement.value = max;
+        
         super(progressBarElement);
-        this.progressBarElement = progressBarElement;
 
         this.position.set( -.2, 2, 0 );
         this.layers.set( 0 );
     }
 
     setAmount(amount) {
-        this.progressBarElement.value = amount
+        this.element.value = amount;
     }
 }
 
