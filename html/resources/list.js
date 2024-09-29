bb0b6c849809dba26667f67cc66e3e60ec6fc30e
jaonorton <jaonorton@gmail.com>
2024-09-28 23:46:16 -0300

    Colocado as coisa

---
+++ html/resources/list.js
@@ -0,0 +1,115 @@
+var list = {
+	"en": {
+		"Projects": {
+			"Basics":  {
+				"Basic Scene": "html/basicSceneInfo",
+				"Using the keyboard": "html/keyboardExampleInfo",
+				"Geometric Transformations 1": "html/geometricTransformation1Info",
+				"Geometric Transformations 2": "html/geometricTransformation2Info",
+				"Geometric Transformations 3": "html/geometricTransformation3Info",            
+				"Projection": "html/projectionInfo",
+				"Virtual Camera": "html/virtualCameraInfo",				
+			},
+			"Animations":  {
+				"Basic": "html/animationInfo",
+				"Using External Objects": "html/animationExternalObjectsInfo",
+				"Linear Interpolation": "html/animationLerpInfo",
+				"Spherical Interpolation": "html/animationQuaternionLerpInfo",          
+			},
+			"Lights & Shadows":  {
+				"Types of lights": "html/lightingTypesInfo",
+				"Spotlight in Details": "html/lightingSpotlightDetailsInfo",				
+				"Shadow Map Types":"html/shadowMapTypesInfo",			
+				"Shadow Map Viewer":"html/shadowMapViewerInfo",
+				"Shadow Update":"html/shadowUpdateInfo",				
+				"Types of materials": "html/materialsAndShadingInfo",            
+			},			
+			"Geometries":  {
+				"Manual Creation": "html/geometriesManualInfo",				
+				"Basic": "html/geometriesBasicInfo",
+				"Lathe": "html/geometriesLatheInfo",
+				"Convex": "html/geometriesConvexInfo",
+            "CSG": "html/geometryCSGInfo",
+				"Loading External Files": "html/geometriesExternalObjectsInfo",
+			},			
+			"Textures":  {
+				"Basic": "html/textureBasicInfo",
+				"Main Options": "html/textureOptionsInfo",
+				"Textured External Objects": "html/textureExternalObjectsInfo",
+            "MultiTextured Cube": "html/texturedMultiMapInfo",
+				"UV Mapping":"html/textureUVMappingInfo",            
+			},
+			"Textures - Advanced":  {
+				"Skybox (Cube)": "html/textureAdvancedSkyboxCubeInfo",
+				"Skybox (Equirectangular)": "html/textureAdvancedSkyboxEquirectangularInfo",            
+				"Skybox (Stripe)": "html/textureAdvancedSkyboxCubeStripeInfo",
+				"Skybox (Cross)": "html/textureAdvancedSkyboxCubeCrossInfo",            
+				"Environment mapping 1": "html/textureAdvancedEnvMapCubeInfo",
+				"Environment mapping 2": "html/textureAdvancedEnvMapEquirectangularInfo",				
+				"Normal mapping": "html/textureAdvancedNormalMappingInfo",								
+				"Displacement mapping": "html/textureAdvancedDisplacementMappingInfo",            		
+			},         
+			"Textures - Extras":  {
+				"Transition": "html/textureExtraTransitionInfo",            
+            "Traversing": "html/textureExtraTraversingInfo",            
+			},         
+			"Realism":  {
+				"Ray Tracing 1": "html/raytracingExample1Info",
+				"Ray Tracing 2": "html/raytracingExample2Info",
+				"Path Tracing 1": "html/pathTracingExample1Info",            
+				"Path Tracing 2": "html/pathTracingExample2Info",                        
+			},			
+		},
+		"XR": {
+			"AR with AR.js":  {
+				"AR - Basic": "html/AR_BasicInfo",	
+				"AR - Sources": "html/AR_ChooseSourceInfo",	            
+				"AR - Multi": "html/AR_MultiInfo",	                        
+				"AR - Multi as One": "html/AR_MultiAsOneInfo",	                                    
+			},
+			"VR with WebXR":  {
+				"VR - Panorama": "html/webxr_VR_PanoramaInfo",				
+				"VR - Teleport": "html/webxr_VR_TeleportInfo",
+				"VR - Free Move": "html/webxr_VR_FreeMoveInfo",				
+				"VR - Dragging": "html/webxr_VR_DraggingInfo",				
+				"VR - Labelling":"html/webxr_VR_LabellingInfo",
+			},
+		},
+		"Additional Examples":{
+			"Physics":  {
+				"Ammo - Vehicle": "html/ammoVehicleInfo",
+				"Ammo - Breakable Objects": "html/ammoBreakableObjectsInfo",
+			},
+			"Utils":  {
+            "GLTF Bounding Box": "html/exampleGLTFBoundingBoxInfo",            
+            "Collision": "html/exampleCollisionInfo",            
+            "Vector Reflection": "html/exampleReflectionInfo",                        
+			},
+			"Raycaster":  {
+				"Drag Control": "html/raycasterDragControlInfo",
+				"Selection": "html/raycasterSelectionInfo",
+            "Layers": "html/raycasterLayersInfo",
+			},   
+			"Geometries - Misc":  {
+				"Cloning": "html/exampleCloningInfo",	
+				"Traverse": "html/exampleTraverseInfo",			
+			},               
+			"Interaction":  { 
+            "Camera Follow Object": "html/exampleCamFollowObjInfo",        
+				"First Person": "html/exampleFirstPersonInfo",
+				"Fly Controls":"html/exampleFlyControlInfo",		
+				"Joystick Elements":"html/exampleJoystickInfo",						
+			},
+			"Shaders":  {
+				"Water": "html/exampleShaderWaterInfo",				
+				"Fire": "html/exampleShaderFireInfo",				            
+			},			
+			"Auxiliary":  {
+				"Loading Screen": "html/exampleLoadingScreenInfo",
+				"Loading Screen with button": "html/exampleLoadingScreenWithButtonInfo",				
+				"Audio": "html/exampleAudioInfo",				
+			},
+
+		},
+	},
+};
